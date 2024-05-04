a7c7b5682770ee332afbb3ad6aa7d4b5454fa567
Brayden Meyer <brayden@Braydens-MacBook-Pro.local>
2024-05-04 02:34:18 -0400

    Initial scaffolding of sidebar and menubar

---
+++ app/navbar.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link"
+import {
+  CircleUserIcon,
+  SearchIcon,
+} from "lucide-react"
+
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+
+export default function Navbar() {
+    return (
+      <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      <div className="w-full flex-1">
+        <form>
+          <div className="relative">
+            <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search gyms and routes..."
+              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+            />
+          </div>
+        </form>
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="icon" className="rounded-full">
+            <CircleUserIcon className="h-5 w-5" />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Your Name</DropdownMenuLabel>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+    );
+}
---
+++ app/sidebar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import {
+  CheckCircle2Icon,
+  MapIcon,
+  MountainIcon,
+  SearchIcon,
+} from "lucide-react"
+import { usePathname } from "next/navigation";
+
+export default function Sidebar() {
+    const pathname = usePathname();
+
+    return (
+        <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <MountainIcon className="h-6 w-6" />
+              <span className="">Grotto</span>
+            </Link>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="/"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Button variant="secondary" className="w-full justify-start bg-emerald-900 text-white">
+                  <SearchIcon className="h-4 w-4" />
+                  Explore
+                </Button>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <MapIcon className="h-4 w-4" />
+                Find a Gym
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <CheckCircle2Icon className="h-4 w-4" />
+                Completed Routes
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-emerald-900">
+                  6
+                </Badge>
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    );
+}
