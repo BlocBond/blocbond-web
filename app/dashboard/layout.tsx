7ba1e61ee572ee40ec210effef66e9a1f257768a
ericbuys <eric.buys02@gmail.com>
2024-05-04 16:36:48 -0400

    Reworked file structure for layout compatability

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import Sidebar from "@/components/sidebar";
+import Navbar from "@/components/navbar";
 import { NextAuthProvider } from "@/app/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +22,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <NextAuthProvider>
           <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <Sidebar />
           <div className="flex flex-col">
+            <Navbar />
             {children}
           </div>
           </div>  
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,25 @@
-"use client";
+"use client"
 
-import GymCard from "@/components/GymCard";
-import NoResults from "./noresults";
-import GymRow from "@/components/GymCardRow";
-import RouteCardRow from "@/components/RouteCardRow";
+import GoogleSignInButton from "@/components/GoogleSignInButton"
+import { useSession } from "next-auth/react"
+import Image from "next/image";
 
-export default function Dashboard() {
-  return (
-    <div className="p-10">
-      <h2 className="text-2xl mb-8">Gyms</h2>
-      <GymRow
-        gyms={[
-          {
-            id: 1,
-            name: "The Guelph Grotto",
-            logoImageUrl: "https://via.placeholder.com/100",
-          },
-          {
-            id: 2,
-            name: "Guelph Gryphons Athletics Centre",
-            logoImageUrl: "https://via.placeholder.com/100",
-          },
-          {
-            id: 1,
-            name: "The Guelph Grotto",
-            logoImageUrl: "https://via.placeholder.com/100",
-          },
-        ]}
-      />
-      <h2 className="text-2xl mt-20 mb-8">Routes</h2>
-      <RouteCardRow
-        routes={[
-          {
-            id: 1,
-            name: "Route 1",
-            imageUrl: "https://via.placeholder.com/300x100",
-          },
-          {
-            id: 2,
-            name: "Route 2",
-            imageUrl: "https://via.placeholder.com/300x100",
-          },
-          {
-            id: 3,
-            name: "Route 3",
-            imageUrl: "https://via.placeholder.com/300x100",
-          },
-        ]}
-      />
-    </div>
-    // <NoResults />
-  );
+export default function Login() {
+    const { status, data: session } = useSession();
+    
+    if (status === "authenticated") {
+        return (
+            <div>
+                {session?.user?.image && (<Image src={session.user.image} alt="Profile Picture" width={60} height={60} />)}
+                <div>Name: <span>{session?.user?.name}</span></div>
+                <div>Email: <span>{session?.user?.email}</span></div>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <GoogleSignInButton />
+            </div>
+        )
+    }
 }
