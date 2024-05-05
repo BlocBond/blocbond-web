242a708a0359a82a4c28f7eed3a98cb42eab7a8f
Brayden Meyer <123148100+brayden-meyer@users.noreply.github.com>
2024-05-04 21:01:02 -0400

    Add redirect to /dashboard on GoogleSignInButton

--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -1,11 +1,17 @@
-"use client"
-
 import Image from "next/image";
 import { signIn } from "next-auth/react";
+import { useRouter} from "next/navigation";
 
 export default function GoogleSignInButton() {
+    const router = useRouter();
+
+    const handleSignIn = async () => {
+        await signIn('google');
+        router.push('/dashboard');
+    };
+
     return (
-        <button onClick={() => signIn('google')} className="flex flex-row justify-center items-center w-full rounded-lg pl-3">
+        <button onClick={handleSignIn} className="flex flex-row justify-center items-center w-full rounded-lg pl-3">
             <Image src="/google-logo.png" alt="Google Logo" height={30} width={30} />
             <span className="bg-blue-500 text-white px-4 py-3">
                 Sign in with Google
