aa0db2bdc79a0b1a6ad7a4da13aa76040c6d81a3
Myron Ladyjenko <mladyjen@uoguelph.ca>
2024-05-05 07:16:38 -0400

    Final

--- app/dashboard/completed-routes/page.tsx
+++ app/dashboard/completed-routes/page.tsx
@@ -2,6 +2,11 @@
 
 export default function CompletedRoutes() {
     return (
-        <h1>This is the completed routes page.</h1>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+            <img src="/BlocBondLogo.png" alt="Description of the image" style={{ maxWidth: '10%', marginRight: '20px' }} />
+            <div>
+                <h1 style={{ fontSize: '3em' }}>To be implemented...</h1>
+            </div>
+        </div>
     )
 }
--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -8,7 +8,7 @@ export default function GoogleSignInButton() {
     };
 
     return (
-        <button onClick={handleSignIn} className="flex flex-row justify-center items-center w-full rounded-lg pl-3">
+        <button onClick={handleSignIn} className="flex flex-row justify-center items-center w-full rounded-lg pl-3 ">
             <Image src="/google-logo.png" alt="Google Logo" height={30} width={30} />
             <span className="bg-blue-500 text-white px-4 py-3">
                 Sign in with Google
